84cad95670463a27d18d324acd79250f8737f579
Scorpio_I4 <18556134754@163.com>
2023-05-15 22:43:33 +0800

    feat: code splitting 生产环境代码分割 over

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig, normalizePath } from 'vite';
+// import { defineConfig, normalizePath, splitVendorChunkPlugin } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
+import inspect from 'vite-plugin-inspect';
 import autoprefixer from 'autoprefixer';
 import svg from 'vite-svg-loader';
-import imagemin from 'vite-plugin-imagemin';
+// import imagemin from 'vite-plugin-imagemin';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 import virtual from './plugins/virtual-module';
 
 // 全局 scss 文件的路径，通过 normalizePath 解决 Windows 下的路径问题
@@ -16,32 +19,39 @@ export default defineConfig({
   base: '/', // 类似 vue-cli 中 publicPath 配置项
   plugins: [
     vue(),
+    inspect(),
     svg(),
+    // splitVendorChunkPlugin(),
     // 压缩图片资源，减小打包的体积【打包时间会随着图片资源的数量提升，对于包体积和打包时间两者的平衡自行把握】
-    imagemin({
-      optipng: {
-        optimizationLevel: 7,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-      },
-      svgo: {
-        plugins: [
-          {
-            name: 'removeViewBox',
-          },
-          {
-            name: 'removeEmptyAttrs',
-            active: false,
-          },
-        ],
-      },
-    }),
+    // imagemin({
+    //   optipng: {
+    //     optimizationLevel: 7,
+    //   },
+    //   pngquant: {
+    //     quality: [0.8, 0.9],
+    //   },
+    //   svgo: {
+    //     plugins: [
+    //       {
+    //         name: 'removeViewBox',
+    //       },
+    //       {
+    //         name: 'removeEmptyAttrs',
+    //         active: false,
+    //       },
+    //     ],
+    //   },
+    // }),
     // 生成雪碧图
     createSvgIconsPlugin({
       iconDirs: [path.join(__dirname, './src/assets/imgs/sprite')],
     }),
     virtual(),
+    chunkSplitPlugin({
+      customSplitting: {
+        vendor: ['vue'],
+      },
+    }),
   ],
   css: {
     postcss: {
@@ -59,6 +69,13 @@ export default defineConfig({
     },
   },
   build: {
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks: {
+    //       vendor: ['vue'],
+    //     },
+    //   },
+    // },
     assetsInlineLimit: 8 * 1024, // 静态资源是否提取成单文件的临界值，小于 8KB 的文件被 base64 内联【svg 始终会打包成单文件】
   },
   resolve: {
